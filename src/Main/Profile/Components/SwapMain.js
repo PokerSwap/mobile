6019309770426eb252ed074480b85cf83039d881
Gabriel Herndon <gherndon5@gmail.com>
2019-10-07 12:48:47 -0400

    Added context consumer

--- src/Main/Profile/Components/SwapMain.js
+++ src/Main/Profile/Components/SwapMain.js
@@ -3,6 +3,7 @@ import {  ListItem, Button, Text } from 'native-base';
 import SwapHead from './SwapHead';
 import SwapBody from './SwapBody';
 import { Grid, Col, Row } from 'react-native-easy-grid'
+import { Context } from "../../../Store/appContext";
 
 export default class SwapMain extends Component {
     constructor(props){
@@ -17,33 +18,24 @@ export default class SwapMain extends Component {
         <ListItem noIndent style={{flex:1, flexDirection:'column'}}>
           <Grid>
             <Col>
-              <SwapHead 
-                tournament={this.props.tournament} 
-                date={this.props.date} year={this.props.year}
-                swaps={this.props.swaps}/>
-              <Row 
-                style={{
-                  marginVertical:20, 
-                  justifyContent:'center' 
-                }}
-              >
-                <Button large bordered info 
-                  style={{
-                    marginTop:10, 
-                    width:'100%', 
-                    justifyContent:'center'
-                  }}
-                >
-                  <Text style={{fontWeight:"600"}}>
-                    {this.props.swaps} SWAPS
-                  </Text>
-                </Button>
-              </Row>
-              
-              {/* TOURNEY BUYIN ENTRIES 
-              <Content.Consumer>
+              {/* SWAP HEAD 
+                W/ TOURNAMENT NAME, START DATE, AND SWAP NUMBERS */}
+              <Context.Consumer>
                 {({store, actions}) => {
-                  return store.swaps.map((content, index) => {
+
+                    <SwapHead 
+                    tournament={this.props.tournament} 
+                    date={this.props.startat}
+                    swaps={this.props.swaps}/>
+                }}
+              </Context.Consumer>
+              
+              {/* SWAP BODY  */}
+              <Context.Consumer>
+                {({store, actions}) => {
+                  // var x = store.swaps.filter(swap => swap.id == this.props.swaps)
+                   return store.my_swaps.map((content, index) => {
+
                     return(
                       <SwapBody
                         id = {content.id}
@@ -54,9 +46,9 @@ export default class SwapMain extends Component {
                     )
                   })
                 }}
-              </Content.Consumer> */}
+              </Context.Consumer>
               
-              </Col>
+            </Col>
           </Grid>
         </ListItem>
       )
