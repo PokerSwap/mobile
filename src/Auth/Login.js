6c7c87a95e2ba8a90c7cb2670c1ce8e066e85ed8
Gabriel Herndon <gherndon5@gmail.com>
2019-10-07 22:34:13 -0400

    building the login functionality

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -1,67 +1,138 @@
 import React, {Component} from 'react';
-import {Button, Text, H3, Item, Input, Body, Left, Right,
-  Card, CardItem, Container, Content} from 'native-base';
+import {Button, Text, H3, Item, Toast, Body, Left, Right,
+Card, CardItem, Container, Content} from 'native-base';
+import { Context } from '../Store/appContext'
+
+import Spinner from 'react-native-loading-spinner-overlay';
+import { Grid, Row, Col } from 'react-native-easy-grid';
+import { View, Image, StatusBar, SafeAreaView, Keyboard, 
+  TouchableWithoutFeedback, TextInput, KeyboardAvoidingView } from 'react-native';
+
 
 export default class LoginScreen extends Component {
     constructor(){
       super();
       this.state={
-  
+        email:'',
+        password:''
       }
     }
+
+    loading = () => {
+      this.setState({loading: !this.state.loading})
+    }
+  
+    startButton = async (x) => {
+      this.loading();
+      var answer = await x.user.login(
+        this.state.username, 
+        this.state.password, 
+        this.props.navigation);
+      console.log('response', answer)
+      this.loading();	
+    }
+
     render(){
       var {navigate} = this.props.navigation
       return(
         <Container>
           <Content contentContainerStyle={{flex:1, justifyContent:"center"}}>
             <Card transparent>
-              {/* Login Title */}
-              <CardItem>
-                <Left />
-                <Body>
-                  <H3>Swap Poker</H3>
-                </Body>
-                <Right />
-
+              
+              {/* TITLE */}
+              <CardItem >
+                <H3>Swap Poker</H3>
               </CardItem>
-              {/* Username/Email Input */}
+
+              {/* EMAIL INPUT */}
               <CardItem>
-                  <Item>
-                    <Input placeholder="Enter Email"/>
-                  </Item>
-                </CardItem>
-              {/* Password Input */}
+                <Item>
+                  <TextInput 
+                    placeholder="Enter Email"
+                    keyboardType="email-address"
+                    blurOnSubmit={false}
+                    returnKeyType="next"
+                    autoCapitalize='none'
+                    autoCorrect={false} 
+                    onSubmitEditing={() => { this.txtPassword.focus(); }}
+                    value={this.state.email}    
+                    onChangeText={email => this.setState({ email })}
+                  />
+                </Item>
+              </CardItem>
+              
+              {/* PASSWORD INPUT */}
               <CardItem>
-                  <Item>
-                    <Input placeholder="Enter Password"/>
-                  </Item>
-                  </CardItem>
-              {/* Sign In Button */}
+                <Item>
+                  <TextInput 
+                    placeholder="Enter Password"
+                    secureTextEntry
+                    autoCapitalize='none'
+                    returnKeyType="go"
+                    autoCorrect={false} 
+                    ref={(input) => { this.txtPassword = input; }} 
+                    value={this.state.password}
+                    onChangeText={password => this.setState({ password })}
+                  />
+                </Item>
+              </CardItem>
+              
+              {/* LOG IN BUTTON */}
               <CardItem>
                   <Left/>
                   <Body>
                     <Button onPress={()=>navigate("Swaps")}>
-                      <Text>Sign In</Text>
+                      <Text>Log In</Text>
                     </Button>
                   </Body>
                   <Right/>
                 </CardItem>
-              {/* Sign Up and Forgot Password Buttons */}
+              
+                {/* LOGIN BUTTON */}
+                <Context.Consumer>
+                  {({store, actions}) => {
+                    wrong = () => {
+                      if(store.userToken==null){
+                        Toast.show({
+                          text:'Sorry you entered the wrong email or password',
+                        duration:3000})
+                      }
+                    }
+                    return(
+                      <View>
+                        <Button block
+                          onPress={
+                            async() => {
+                              Keyboard.dismiss();
+                              var x = await this.startButton(actions)
+                              wrong()}
+                            }
+                        >
+                          <Text> Login</Text>
+                        </Button>
+                      </View>
+                    )
+                  }}
+                </Context.Consumer>
+
+
+              {/* BOTTOM BUTTONS */}
               <CardItem>
                   <Left>
                     <Body>
-                    <Button transparent>
-                      <Text>Forgot password?</Text>
-                    </Button>
+                      {/* FORGOT PASSWORD BUTTON */}
+                      <Button transparent>
+                        <Text>Forgot password?</Text>
+                      </Button>
                     </Body>
                   </Left>
                   <Right>
                   <Body>
-
+                    {/* SIGN UP BUTTON */}
                     <Button transparent onPress={()=>navigate("TermsAndConditions")}>
                       <Text>First Time</Text>
                     </Button>
-                    </Body>
+                  </Body>
 
                   </Right>
                 </CardItem>
