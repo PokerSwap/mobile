e2134ebdd9fcc0aa2d576f1133f134d794d89de7
Gabriel Herndon <gherndon5@gmail.com>
2019-10-18 13:11:52 -0400

    minor change

--- src/Main/Swaps/SwapDashboard.js
+++ src/Main/Swaps/SwapDashboard.js
@@ -18,6 +18,7 @@ export default class SwapDashboard extends Component {
           <_Header drawer={() => this.props.navigation.toggleDrawer()}/>
           <Content>
             <List>
+
               {/* LIVE SWAPS LIST HEADER */}
               <Separator bordered style={{height:48, backgroundColor:'rgb(56,68,165)'}}>
                 <Text style={{fontSize:20, color:'white', fontWeight:'600', textAlign:'center'}}> 
@@ -25,36 +26,6 @@ export default class SwapDashboard extends Component {
                 </Text>
               </Separator>
 
-              {/* LIVE SWAPS LIST */}
-              {/* <Context.Consumer>
-                {({store, actions})=> {
-                  var tournaments
-                  return torunaments.map((content, index) => {
-                    return(
-                      <Tournament 
-                        name={content.name}
-                      />
-                  )})}}
-              </Context.Consumer>  */}
-
-              {/* INCOMING SWAPS LIST */}
-              {/* <Context.Consumer>
-                {({store, actions})=> {
-                  var incomingSwaps = store.my_swaps.filter(item => item.state =='incoming')
-                  return incomingSwaps.map((content, index) => {
-                    return(
-                      <BuyIn 
-                        key={index}
-                        name={content.name}
-                        percent={content.percent}
-                        table={content.table}
-                        seat={content.seat}
-                        chips={content.chips}
-                        offer={content.offer}
-                      />
-                  )})}}
-              </Context.Consumer>  */}
-  
               {/* SCHEDULED SWAPS LIST HEADER */}
               <Separator bordered style={{height:48, backgroundColor:'gray'}}>
                 <Text style={{fontSize:20, color:'white', fontWeight:'600', textAlign:'center'}}> 
@@ -62,42 +33,25 @@ export default class SwapDashboard extends Component {
                 </Text>
               </Separator>
 
-                {/* SCHEDULED TOURNAMENT TITLE AND BOX */}
-                <Context.Consumer>
-                  {({store, actions})=> {
-                    var e = store.profile_in_session.buy_ins
-                    return e.map((content, index) => {
-
-                      return(
-                        <SwapTracker
-                          tournament = {content.flight.tournament}
-                        />
-                      )
-                      })
-                    }}
-                  </Context.Consumer>
+              {/* SCHEDULED TOURNAMENT TITLE AND BOX */}
+              <Context.Consumer>
+                {({store, actions})=> {
+                  var mySwaps = store.profile_in_session.receiving_swaps
+                  return mySwaps.map((content, index) => {
+                    var x = content.tournament_id;
+                    var tournament = () => actions.tournament.get(x)
+                    return(
+                      <SwapTracker
+                        tournament = {tournament.name}
+                      />
+                    )
+                    })
+                  }}
+              </Context.Consumer>
                     
-                  {/* MY BUYIN */}
-
-                  {/* RELATED BUYIN AND SWAP */}
-                  {/* <Context.Consumer>
-                  {({store, actions})=> {
-                    var mySwaps = store.profile_in_session.receiving_swaps;
-                    return mySwaps.map((content, index) => {
-
-                      return(
-                        <BuyIn
-                          key={index} 
-                          percentage={content.percentage}
-                          first_name={content.user.first_name}
-                          last_name={content.user.last_name}
-                        />
-                    )})}}
-                </Context.Consumer>   */}
-
             </List>
           </Content>
         </Container>
-      )
-    }
+    )
+  }
 }
