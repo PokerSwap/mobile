eb9d1f9ba5a8aba52b33cffca72bbbdfb2237783
Gabriel Herndon <gherndon5@gmail.com>
2019-10-24 20:50:42 -0400

    added profile head and made logout visible

--- src/View-Components/sidebar.js
+++ src/View-Components/sidebar.js
@@ -1,33 +1,61 @@
 import React from 'react'
 import { DrawerItems } from 'react-navigation'
-import { Container, Content, Text, Header, Button } from 'native-base'
-import {Alert} from 'react-native'
+import { Container, Content, Text, Icon, Button } from 'native-base'
+import {Alert, View} from 'react-native'
+
+import ProfileBio from '../Main/Profile/Components/ProfileBio'
+import { Context } from '../Store/appContext'
 
 // DRAWER PHYSICAL COMPONENT
 const SideBar = props => (
   <Container>
-    <Header><Text> Swap Profit </Text></Header>
+    
+
+      
+
     <Content>
       
+    <Context.Consumer>
+        {({ store, actions }) => {
+          var user = store.profile_in_session
+            return(
+              <ProfileBio 
+            first_name={user.first_name}
+            last_name={user.last_name}
+            username={user.username}
+            roi={user.roi}
+            rating={user.rating}
+            picture={user.picture}
+          />
+            )
+          }
+        }
+      </Context.Consumer>
       {/* DEFAULT BUTTONS */}
       <DrawerItems {...props} />
       
       {/* LOGOUT OPTION */}
-      <Button transparent onPress={()=>
-              Alert.alert(
-                'Log out',
-                'Do you want to logout?',
-                [
-                  {text: 'Cancel', onPress: () => {return null}},
-                  {text: 'Confirm', onPress: () => {
-                    props.navigation.navigate('LogIn')
-                  }},
-                ],
-                { cancelable: false }
-              )  
-            }>
-              <Text style={{margin: 16,fontWeight: 'bold'}}>Logout</Text>
-            </Button>
+      <View>
+
+        <Button transparent
+          style={{justifyContent:'flex-start'}}
+          onPress={()=>
+          Alert.alert(
+            'Log out',
+            'Do you want to logout?',
+            [
+              {text: 'Cancel', onPress: () => {return null}},
+              {text: 'Confirm', onPress: () => {
+                props.navigation.navigate('LogIn')
+              }},
+            ],
+            { cancelable: false }
+          )  
+        }>
+          <Icon name='exit'/>
+          <Text style={{fontWeight: 'bold'}}>Logout</Text>
+        </Button>
+      </View>
     
     </Content>
   </Container>
