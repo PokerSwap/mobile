6040de890fa93733f8bb8ff52e7d7f7aa6f2bf5c
GGabe <gherndon5@gmail.com>
2019-12-13 16:29:56 -0500

    added rnp

--- App.js
+++ App.js
@@ -2,6 +2,20 @@ import React, {} from 'react';
 import {Root} from 'native-base';
 import AppContainer from './AppContainer.js'
 
+
+const PaymentRequest = require('react-native-payments').PaymentRequest;
+console.log(PaymentRequest)
+
+const METHOD_DATA = [{
+  supportedMethods: ['apple-pay'],
+  data: {
+    merchantIdentifier: 'merchant.com.swapprofitllc.swapprofit',
+    supportedNetworks: ['visa', 'mastercard', 'amex'],
+    countryCode: 'US',
+    currencyCode: 'USD'
+  }
+}];
+
 export default  App = () => {
   return(
     <Root>
