feb69b594ca1d183b8d74f04fc2f4ac0de271e15
GGabe <gherndon5@gmail.com>
2019-12-13 16:26:55 -0500

    added deviceID

--- src/Auth/SignUp/CreateUser.js
+++ src/Auth/SignUp/CreateUser.js
@@ -1,9 +1,16 @@
-import React, {Component, useState} from 'react';
+import React, {useContext, useState} from 'react';
 import { TextInput } from 'react-native'
 import { Button, Container, Card, CardItem, Item, Input, Icon, Text } from 'native-base';
 import { Context } from '../../Store/appContext';
+import DeviceInfo from 'react-native-device-info'
+
+import { getUniqueId, getManufacturer } from 'react-native-device-info'
 
 export default CreateUser = (props) => {
+
+	var deviceID = DeviceInfo.getUniqueId();
+
+	const { store, actions } = useContext(Context)
 
 	const [ loading, setLoading] = useState(false)
 	const [ email, setEmail ] = useState('')
@@ -17,6 +24,7 @@ export default CreateUser = (props) => {
 			<Card transparent style={{justifyContent:'center'}}>
 				<CardItem>
 					<Text>A validation link has been sent to your email at {email}.</Text>
+					<Text>{deviceID}</Text>
 				</CardItem>
 			</Card>
 	} else {
@@ -25,7 +33,7 @@ export default CreateUser = (props) => {
 				<CardItem style={{flexDirection:"column", paddingLeft: 30, width:350, justifyContent:"center"}}>
 					<Text style={{textAlign:"center", width:300, fontSize:20}}>
 						Please create your Username for when you login and 
-						your personal Password and your Email Address.
+						your personal Password and your Email Address.{deviceID}
 					</Text>
 					<Item>
 						<Icon active name='mail' />
@@ -58,7 +66,7 @@ export default CreateUser = (props) => {
 				
 				<CardItem footer style={{justifyContent:"center"}}>
 					<Button large onPress={() => {
-						actions.user.add(email, password);
+						actions.user.add(email, password, deviceID);
 						setSubmitted(true)
 					}}>
 						<Text> SUBMIT </Text>
