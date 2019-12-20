335a2999c3e8181d2697bce19833201cb8dc59a6
GGabe <gherndon5@gmail.com>
2019-12-20 10:31:08 -0500

    Renderal conditioning works with coins

--- src/Main/SwapOffer/Paths/inactive.js
+++ src/Main/SwapOffer/Paths/inactive.js
@@ -8,6 +8,7 @@ import {Context} from '../../../Store/appContext'
 export default InactivePath = (props) => {
 
   const {store, actions} = useContext(Context)
+  
 
   return(
     <Card>
@@ -20,40 +21,51 @@ export default InactivePath = (props) => {
         <Text style={{marginLeft:5,fontSize:36,justifyContent:'center'}}> {props.user_name} </Text>
       </CardItem>
       
-      <CardItem style={{justifyContent:'center'}}> 
-        <Text style={{marginRight:5,fontSize:24,justifyContent:'center'}}>Swap Offer:</Text>
-      </CardItem>
+      {store.my_profile.coins > 0 ? 
+        <View>
+          <CardItem style={{justifyContent:'center'}}>
+            {/* SUBTRACT BUTTON */}
+            <TouchableOpacity onPress={()=> props.subtract()}>
+              <View style={{width:50, height:50, borderRadius: 5, backgroundColor:'blue'}}>
+                <Text style={{fontSize:36, color:'white', textAlign:'center'}}>-</Text>
+              </View>
+            </TouchableOpacity>
 
-      <CardItem style={{justifyContent:'center'}}>
+            {/* SWAP PERCENTAGE */}
+            <Text style={{fontSize:36, marginHorizontal:10}}> {props.percentage}% </Text>
 
-        {/* SUBTRACT BUTTON */}
-        <TouchableOpacity onPress={()=> props.subtract()}>
-          <View style={{width:50, height:50, borderRadius: 5, backgroundColor:'blue'}}>
-            <Text style={{fontSize:36, color:'white', textAlign:'center'}}>-</Text>
-          </View>
-        </TouchableOpacity>
-        
-        {/* SWAP PERCENTAGE */}
-        <Text style={{fontSize:36, marginHorizontal:10}}> {props.percentage}% </Text>
-        
-        {/* ADD BUTTON */}
-        <TouchableOpacity onPress={()=> props.add()}>
-          <View style={{width:50, height:50, borderRadius: 5, backgroundColor:'blue'}}>
-            <Text style={{fontSize:36, color:'white', textAlign:'center'}}>+</Text>
-          </View>
-        </TouchableOpacity>
+            {/* ADD BUTTON */}
+            <TouchableOpacity onPress={()=> props.add()}>
+              <View style={{width:50, height:50, borderRadius: 5, backgroundColor:'blue'}}>
+                <Text style={{fontSize:36, color:'white', textAlign:'center'}}>+</Text>
+              </View>
+            </TouchableOpacity>
 
-      </CardItem>
+          </CardItem>
+          <CardItem style={{justifyContent:'center'}}>
+            <Button large 
+              onPress={async() => {
+                var answer = await actions.swap.add(props.tournament_id, props.user_id, props.percentage);
+                actions.tracker.getAll()
+              }}>
+              <Text> Offer Swap </Text>
+            </Button>
+          </CardItem>
+        </View>
+        : 
+        <View>
+          <CardItem>
+            <Text> You need to purchase tokens in order to swap with this person.</Text>
+          </CardItem>
 
-      <CardItem style={{justifyContent:'center'}}>
-        <Button large 
-          onPress={async() => {
-            actions.swap.add(tournament_id, user_id, percentage),
-            actions.profile.get()
-          }}>
-          <Text> Offer Swap </Text>
-        </Button>
-      </CardItem>
+          <CardItem>
+            <Button large success onPress={()=> props.navigation.navigate('PurchaseTokens')}>
+              <Text>Purchase Tokens</Text>
+            </Button>
+          </CardItem>
+        </View>
+      }
+
 
     </Card>
   )
--- src/Main/SwapOffer/Paths/incoming.js
+++ src/Main/SwapOffer/Paths/incoming.js
@@ -1,35 +1,75 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import {TextInput} from 'react-native'
 import {Text, Card, Button, CardItem} from 'native-base'
 import {Row} from 'react-native-easy-grid'
 
+import {Context} from '../../../Store/appContext'
+
 export default IncomingPath = (props) => {
+
+  const {store, actions } = useContext(Context)
+
+  var swapChange = async(x) => {
+    var answer = await actions.swap.statusChange(
+      props.tournament_id, 
+      props.user_id,
+      false,
+      //status 
+      x,
+      props.percentage,
+      props.counter_percentage
+    )
+    var answer2 = await actions.coin.spend()
+    var answer3 = await actions.tracker.getAll()
+    props.navigation.goBack()
+  }
 
   return(
     <Card>
 
-      <CardItem>
-        <Row style={{justifyContent:'center'}}><Text> Swap Offer: </Text></Row>
-        <Row style={{justifyContent:'center'}}><Text> {props.percentage}% </Text></Row>
-      </CardItem>
+      {props.percentage == props.counter_percentage ?
+        <CardItem>
+          <Text>{props.user_name} wants to swap {props.percentage}% between the both of you</Text>
+        </CardItem>
+        : 
+        <CardItem>
+          <Text>{props.user_name}% wants to swap {props.counter_percentage}% while you swap {props.percentage}%</Text>
+        </CardItem>
+      }
+
+      {store.my_profile.coins > 0 ?
+        <View>
+          <CardItem>
+            <Button success
+              onPress={()=> swapChange("agreed")}>
+              <Text> Accept Offer </Text>
+            </Button>
+          </CardItem>
+        
+          <CardItem>
+            <Button warning>
+              <Text> Counter Offer </Text>
+            </Button>
+          </CardItem>
+        </View>
+        :
+        <View>
+          <CardItem>
+            <Text> In order to accept or counter this swap, you need to purchase tokens.</Text>
+          </CardItem>
+        
+          <CardItem>
+            <Button large success onPress={() => props.navigation.navigate('PurchaseTokens')}>
+              <Text>Purchase Tokens</Text>
+            </Button>
+          </CardItem>
+        </View>
+      }
 
       <CardItem>
-        <Button success
-          onPress={()=> props.swapUpdate()}>
-          <Text> Accept Offer </Text>
-        </Button>
-      </CardItem>
-
-      <CardItem>
-        <Button warning>
-          <Text> Counter Offer </Text>
-        </Button>
-      </CardItem>
-
-      <CardItem>
-        <Button danger>
-          <Text> No Thanks </Text>
+        <Button danger onPress={()=> swapChange("rejected")}>
+          <Text> Reject Swap </Text>
         </Button>
       </CardItem>
 
