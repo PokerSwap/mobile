f988f2acd9c2f1ad6f56646d3c3b0063f3f24c3c
GGabe <gherndon5@gmail.com>
2019-12-20 10:21:18 -0500

    pending is functional

--- src/Main/SwapOffer/Paths/pending.js
+++ src/Main/SwapOffer/Paths/pending.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {TextInput} from 'react-native'
 import {Text, Card, Button, CardItem} from 'native-base'
 
+import {Context} from '../../../Store/appContext'
+
 export default PendingPath = (props) => {
+
+  const { store, actions } = useContext(Context)
+
+
+  var cancelSwap = async() => {
+    var answer = await actions.swap.statusChange(
+      props.tournament_id, 
+      props.user_id,
+      false,
+      //status 
+      "canceled",
+      props.percentage,
+      props.counter_percentage
+    )
+    var answer2 = await actions.tracker.getAll()
+    props.navigation.goBack()
+  }
 
   return(
     <Card>
-      <CardItem>
-        <Text>{props.user_name}</Text>
-        <Button>
+      <CardItem style={{justifyContent:'center'}}>
+        <Text>Swap with {props.user_name} is pending</Text>
+      </CardItem>
+      <CardItem style={{justifyContent:'center'}}>
+        <Text>{props.percentage}%</Text>
+      </CardItem>
+      <CardItem style={{justifyContent:'center'}}>  
+        <Button onPress={()=> cancelSwap()}>
           <Text>Cancel</Text>
         </Button>
       </CardItem>
