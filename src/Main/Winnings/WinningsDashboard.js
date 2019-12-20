39e470789b7384aa6043a1c924e6981b328573a9
GGabe <gherndon5@gmail.com>
2019-12-20 10:17:37 -0500

    history and recent are successfully mapped

--- src/Main/Winnings/WinningsDashboard.js
+++ src/Main/Winnings/WinningsDashboard.js
@@ -4,24 +4,53 @@ import _Header from '../../View-Components/header'
 import { Context } from '../../Store/appContext'
 import WinningsTracker from './Components/WinningsTracker'
 
+import moment from 'moment'
+
 export default WinningsDashboard = (props) => {
 
   const {store, actions} = useContext(Context)
 
-  var recentTracker;
+  var recentTracker ;
+  var historyTracker;
 
-  if(store.pastTrackers.length != 0){
-    recentTracker = store.pastTrackers.map((content, index) => {
+  let noTracker = (f) =>  {
+    return(
+    <ListItem noIndent style={{justifyContent:'center'}}>
+      <Text style={{
+        justifyContent:'center', textAlign:'center', 
+        fontSize:24, width:'80%'}}> 
+        You have no {f} swaps at the moment. 
+      </Text>
+    </ListItem>
+    )  
+}
+
+  let aTracker = (e) => e.map((content, index) => {
     return(
       <WinningsTracker 
-        tournament={content.tournament}
-        my_buyin={content.my_buyin}
-        swaps={content.swaps}
-        navigation={props.navigation}
-      />
-    )})
+          tournament={content.tournament}
+          my_buyin={content.my_buyin}
+          swaps={content.swaps}
+          navigation={props.navigation}
+        />
+    )
+  })
+
+  if(store.pastTrackers.length !== 0){
+    
+    var recentSwaps = store.pastTrackers.filter(
+      tracker => moment().isBefore(moment(tracker.tournament.end_at).add(1, 'months')))    
+    recentSwaps.length !== 0  ? 
+      recentTracker = aTracker(recentSwaps) : recentTracker = noTracker('recent')
+
+    var historySwaps = store.pastTrackers.filter(
+      tracker => moment().isAfter(moment(tracker.tournament.end_at).add(1, 'months')))
+    historySwaps.length !== 0 ? 
+      historyTracker = aTracker(historySwaps) : historyTracker = noTracker('history')
+
   }else{
-    <ListItem><Text>No Recent Winnings</Text></ListItem>
+    recentTracker = noTracker('recent')
+    historyTracker = noTracker('history')
   } 
 
   return(
@@ -41,7 +70,8 @@ export default WinningsDashboard = (props) => {
             <Text style={{fontSize:20, color:'white', fontWeight:'600', textAlign:'center'}}> 
               HISTORY 
             </Text>                
-          </Separator>       
+          </Separator>  
+          {historyTracker}     
         </List>
       </Content>
     </Container>
