bc477c660c6240269e246c8562c8e4538827a724
Gabriel Herndon <gabe@Gabriels-MacBook-Pro.local>
2020-01-03 08:14:16 -0500

    added permissions and geolocation

--- ios/SwapProfit/AppDelegate.m
+++ ios/SwapProfit/AppDelegate.m
@@ -25,8 +25,9 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"SwapProfit"
                                             initialProperties:nil];
-  [FIRApp configure];
-
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
   
 
--- src/Main/Tournaments/Components/TournamentSort.js
+++ src/Main/Tournaments/Components/TournamentSort.js
@@ -2,14 +2,22 @@ import React from 'react'
 
 import { Button, Segment, Text} from 'native-base';
 
+import Geolocation from '@react-native-community/geolocation';
+
+
 export default TournamentSort = (props) => {
+
+  const s = async() => {
+    var sss =  Geolocation.getCurrentPosition(info => console.log(info));
+
+  }
 
   return(
     <Segment >
       <Button first active onPress={()=> props.setSort('Zip Code')}>
         <Text>Zip Code</Text>
       </Button>
-      <Button last onPress={()=> {props.setSort('Current Location')}}>
+      <Button last onPress={()=> s()}>
         <Text>Current Location</Text>
       </Button>
     </Segment>
