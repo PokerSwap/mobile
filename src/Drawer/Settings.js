a8014930db5552a5907701e047ea9f252cf3b62a
GGabe <gherndon5@gmail.com>
2020-01-22 19:30:29 -0500

    prepping

--- src/Drawer/Settings.js
+++ src/Drawer/Settings.js
@@ -1,14 +1,35 @@
 import React, {useContext} from 'react';
+import {Alert} from 'react-native';
 import { Container, Content, Text, List, ListItem } from 'native-base';
 import _Header from '../View-Components/header'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import {Context } from '../Store/appContext'
 
+
+
+
 export default SettingsScreen = (props) => {
 
   const { store, actions } = useContext(Context)
   
+  // const showAlert = (message) =>{
+  //   Alert.alert(
+  //     "Confirmation",
+  //     'Are you sure this is you?',
+  //     [
+  //       {
+  //         text: 'Yes',
+  //         onPress: () => props.next()
+  //       },
+  //       {
+  //         text: 'No',
+  //         onPress: () => console.log("Cancel Pressed"),
+  //       }
+  //     ]
+  //   )
+  // }
+
   return(
     <Container>
       <_Header title={'Settings'} 
