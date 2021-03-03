9b4413af8a7eed5aa90aaadcd88da3466e7571f6
GGabe <gherndon5@gmail.com>
2021-03-03 12:27:58 -0500

    change to store

--- src/Store/store.js
+++ src/Store/store.js
@@ -9,11 +9,11 @@ import firebase from 'firebase'; // 4.8.1
 import { initial } from 'lodash';
 var databaseURL
 
-Platform.OS == 'ios' ?
-	databaseURL = 'http://gabriels-imac.local:3000/' : databaseURL = 'http://10.0.2.2:3000/'
+// Platform.OS == 'ios' ?
+// 	databaseURL = 'http://gabriels-imac.local:3000/' : databaseURL = 'http://10.0.2.2:3000/'
 
 
-// databaseURL = 'https://swapprofit-beta.herokuapp.com/'
+databaseURL = 'https://swapprofit-beta.herokuapp.com/'
 
 
 var errorMessage = (error) => {
